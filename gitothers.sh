3ceb86cb0c09d3782b1c6f882e260375fecdd4c4
HamzMalk <97640361+HamzMalk@users.noreply.github.com>
2023-02-18 18:51:11 -0500

    Add files via upload

---
+++ gitothers.sh
@@ -0,0 +1,10 @@
+echo "# Git-Others" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:HamzMalk/Git-Others.git
+git push -u origin main
+git init
+git add class.md
+git commit
